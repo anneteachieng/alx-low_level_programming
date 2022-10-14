d45c05567f3f059980d664fdcf0fa0994c2e1a84
anneteachieng <achiengomondi02@gmail.com>
2022-10-14 13:26:10 -0700

    My c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point for the program
- *
- * Return: 0 Success
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
+	return (0);
 }
