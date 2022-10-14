1478e08b2285a84d84e48ab6139df61fefa2c386
anneteachieng <achiengomondi02@gmail.com>
2022-10-14 13:09:17 -0700

    My c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Return: 0 if success
  */
 int main(void)
 {
-	puts("\"Programming is like buildinga multilingual puzzle");
+	prinf("with proper grammar, butthe outcome is a piece of art, \n");
 	return (0);
 }
