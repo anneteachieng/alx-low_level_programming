2b74d1846798410feedb683fc74f2f329b9fb6f1
anneteachieng <achiengomondi02@gmail.com>
2022-10-18 14:15:10 -0700

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,10 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
+	int ten;
 	char lett;
 
-	while (count++ <= 9)
+	for (ten = 0; ten <= 9; ten++)
 	{
 		for (lett = 'a'; lett <= 'z'; lett++)
 			_putchar(lett)
