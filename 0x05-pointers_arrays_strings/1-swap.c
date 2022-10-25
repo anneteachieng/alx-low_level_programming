4861c24b11815939abc2a035363b9c09ed6456ef
anneteachieng <achiengomondi02@gmail.com>
2022-10-25 12:54:19 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,17 +2,15 @@
 
 /**
  * swap_int - swaps the values of a and b
- * @a: pointer int = 98
- * @b: pointer int = 42
- * @a: pointer 1
- * @b: pointer 2
+ * @a: int a
+ * @b: int b
  * Return: 0
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
-	*a += *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
 }
