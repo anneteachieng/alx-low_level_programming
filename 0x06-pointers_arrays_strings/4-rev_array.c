2b866caa179debfcdee987a6c8353a3fb33fc49c
anneteachieng <achiengomondi02@gmail.com>
2022-10-28 15:14:56 -0700

    four

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,22 +10,22 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp, b, l;
+	int temp, s, e;
 
-	b = 0;
-	l = n - 1
-		/**
-		 * set value in array a in temp
-		 * then place last array in
-		 * the first array the place
-		 * value in temp to last array
-		 */
-		while (b < l)
-		{
-			temp = a[b];
-			a[b] = a[l];
-			a[l] = temp;
-			a++;
-			l--;
+	s = 0;
+	e = n - 1;
+	/**
+	 * set value in array a in temp
+	 * then place last array in
+	 * the first array the place
+	 * value in temp to last array
+	 */
+	while (s < e)
+	{
+		temp = a[s];
+		a[s] = a[e];
+		a[e] = temp;
+		s++;
+		e--;
 		}
 }
