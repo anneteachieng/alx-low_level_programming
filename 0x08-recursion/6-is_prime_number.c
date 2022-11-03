08b0c449910505577384a4f591655946353beb90
anneteachieng <achiengomondi02@gmail.com>
2022-11-03 13:16:31 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,38 +1,30 @@
 #include "main.h"
 
 /**
- * is_prime_number -  Returns the natural square root of a number.
- * @n: int type
- * Return: char type
+ * check - checks to see if number is prime
+ * @a: int
+ * @b: int
+ * Return: int
  */
-
-int is_prime_number(int n)
+int check(int a, int b)
 {
-	if (n <= 1)
-	{
+	if (b < 2 || b % a == 0)
 		return (0);
-	}
-	return (is_prime(n, 2));
+	else if (a > b / 2)
+		return (1);
+	else
+		return (check(a + 1, b));
 }
 
 /**
- * is_prime - Entry point
- * Desc: is_prime
- * @n: type int
- * @x: type int
- * Return: Function about prime numbers
+ * is_prime_number - state if number is prime
+ * @n: int
+ * Return: int
  */
-
-int is_prime(int n, int x)
+int is_prime_number(int n)
 {
-	if (x < n)
-
-	{
-		if (n % x == 0)
-		{
-			return (0);
-		}
-		return (is_prime(n, x + 1));
-	}
-	return (1);
+	if (n == 2)
+		return (1);
+	return (check(2, n));
 }
+
